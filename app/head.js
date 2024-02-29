7efc546ee74c5c84f4edfbc4e11c471d19b4dfb8
Saurabh Shukla <saurabh1258.be21@chitkarauniversity.edu.in>
2024-02-29 23:42:30 +0530

    get-started section completed

--- app/head.js
+++ app/head.js
@@ -1,9 +1,11 @@
-const Head = () => (
-  <>
+import Head from 'next/head';
+
+const CustomHead = () => (
+  <Head>
     <title>Metaversus</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/favicon.ico" />
-  </>
+  </Head>
 );
 
-export default Head;
+export default CustomHead;
